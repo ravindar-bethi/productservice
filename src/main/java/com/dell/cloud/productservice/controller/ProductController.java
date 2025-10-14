1af74afd38e85420894c400dbd791d8500c39a65
B Ravindar Reddy <ravimca24@gmail.com>
2025-10-14 23:21:23 +0530

    added productservice

---
+++ src/main/java/com/dell/cloud/productservice/controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.dell.cloud.productservice.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dell.cloud.productservice.entity.Product;
+import com.dell.cloud.productservice.service.ProductService;
+
+@RestController
+public class ProductController {
+	private final ProductService productService;
+	
+	public ProductController(ProductService productService) {
+		this.productService=productService;
+	}
+	
+	@GetMapping
+	public List<Product> getAllProducts() {
+		return productService.getAllProducts();
+	}
+	
+	@GetMapping("/{id}")
+	public Product getProduct(@PathVariable String id) {
+		return productService.getProductById(id);
+	}
+	@PostMapping
+	public Product addProduct(@RequestBody Product product){
+		return productService.addProduct(product);
+	}
+	
+	@PutMapping("/{id}/stock")
+	public Product updateStock(@PathVariable String id, @RequestParam int stock) {
+		return productService.updateStock(id, stock);
+	}
+}
---
+++ src/main/java/com/dell/cloud/productservice/service/ProductService.java
@@ -0,0 +1,40 @@
+package com.dell.cloud.productservice.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.dell.cloud.productservice.entity.Product;
+import com.dell.cloud.productservice.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+	private final ProductRepository productRepository;
+
+	public ProductService(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+
+	public List<Product> getAllProducts() {
+		return productRepository.findAll();
+	}
+
+	public Product getProductById(String id) {
+		return productRepository.findById(id).orElse(null);
+	}
+
+	public Product addProduct(Product product) {
+		return productRepository.save(product);
+	}
+
+	public Product updateStock(String id, int stock) {
+		Product product = productRepository.findById(id).orElse(null);
+		if (product != null) {
+			product.setStock(stock);
+			return productRepository.save(product);
+		}
+		return null;
+	}
+
+}
